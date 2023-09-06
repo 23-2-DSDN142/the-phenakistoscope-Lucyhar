924a282ae44967fcad8249923a14a0ea9d8705d0
Lucyhar <140354012+Lucyhar@users.noreply.github.com>
2023-09-07 11:58:34 +1200

    Update my_phenakistoscope.js
    changed background colour and added beach around the outside

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 15;
+const SLICE_COUNT = 18;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -9,32 +9,50 @@ function setup_pScope(pScope){
 }
 
 function setup_layers(pScope){
-
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
-
-  var layer1 = new PLayer(faces);
+  
+  
+  var layer1 = new PLayer(faces, bubbleBack);
   layer1.mode( SWIRL(5) );
   layer1.set_boundary( 200, 1000 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
+
   
+}
+
+function bubbleBack(animation, pScope){
+  pScope.fill_background(0,84,119);
+  noFill();
+  strokeWeight(150);
+  stroke('#F0EDB4'); //sand  colour 
+  ellipse(0, 0, 2000);
+  
+
+
+
 }
 
 function faces(x, y, animation, pScope){
   translate(0, 0);
   scale(animation.frame*2);
 
+
+
 // bubbles
 strokeWeight(2);
 stroke(90, 184, 242); //darker blue 
 fill(212, 238, 255);
- circle(100, 130,30); 
- circle(150, 110,40); 
- circle(60, 110,20); 
- circle(190, 120,20); 
+ circle(10, 30,30); 
+ circle(55, 5,40); 
+ circle(30, -55,20); 
+ circle(-50, 80,20); 
 }
+
+
+
 function squares(x, y, animation, pScope){
 
   // this is how you set up a background for a specific layer
@@ -42,13 +60,26 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(42,75,90)
+  fill(9, 60, 82)
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
   fill("#A7F1A8")
   rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
 
-  //cherrys 1
-fill('#008000' );//green leaf
-ellipse(72, 122, 20,85); // leaf
+  // Body
+  fill(255, 165, 0); // Goldfish color (orange)
+  ellipse(200, 200, 150, 80);
+
+  // Tail
+  fill(255, 0, 0); // Red color for tail
+  triangle(150, 200, 100, 175, 100, 225);
+
+  // Eye
+  fill(0); // Black color for eye
+  ellipse(225, 190, 20, 20);
+
+  // Mouth
+  noFill();
+  stroke(0); // Black color for mouth
+  arc(200, 215, 40, 20, 0, PI);
 }
